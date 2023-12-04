10501204bb29a6a357e7156bfde23e38f7a54504
Matthew Zakari <zakarimatthew62@gmail.com>
2023-12-04 08:02:17 -0800

    python-data_structures task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    # loop through the list in reverse order
+    for i in range(len(my_list) - 1, -1, -1):
+        # use str.format() to print the integer
+        print("{:d}".format(item))
