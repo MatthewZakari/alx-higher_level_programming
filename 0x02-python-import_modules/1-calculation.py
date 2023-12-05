b3c63ab45e7f9f07f26bffd8d84f6c94a5ce953f
Matthew Zakari <zakarimatthew62@gmail.com>
2023-12-05 06:59:25 -0800

    0x02-python-import_modules task 0 & 1 correcton

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,14 +8,7 @@ if __name__ == "__main__":
     b = 5
 
     # Call each imported function and print the result with string formatting
-    add_result = add(a, b)
-    print("{} + {} = {}".format(a, b, add_result))
-
-    sub_result = sub(a, b)
-    print("{} - {} = {}".format(a, b, sub_result))
-
-    mul_result = mul(a, b)
-    print("{} * {} = {}".format(a, b, mul_result))
-
-    div_result = div(a, b)
-    print("{} / {} = {}".format(a, b, div_result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
