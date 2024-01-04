72c341fddbbf23cb2ab21061f4416634f70efda0
Matthew Zakari <zakarimatthew62@gmail.com>
2024-01-04 12:26:48 -0800

    0-rectangle.py modified

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class"""
+
 
 class Rectangle:
+    """
+    This class represents a rectangle.
+
+    Attributes:
+        No attributes for now.
+    """
+
     pass
