7cc50e9a5008bf6e92c2c3d3e9e8024b870f90a3
Matthew <zakarimatthew62@gmail.com>
2024-01-08 13:49:38 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object."""
-    return [name for name in dir(obj) if not name.startswith('__')]
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        A list of attribute and method names.
+    """
+    return [name for name in dir(obj) if not callable(getattr(obj, name)) and not name.startswith('__')]
