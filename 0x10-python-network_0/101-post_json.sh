2010da56cf946db436ff606586896fbd374c67ef
Matthew Zakari <zakarimatthew62@gmail.com>
2024-05-14 08:00:48 -0700

    python method correction

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a specified URL with the contents of a file as the request body
-
 curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
-
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script sends a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!" in the body
-
 curl -s -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
-
