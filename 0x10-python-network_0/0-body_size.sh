2e19a590d29a15092477cdfd90bf6fbd46f13d5a
Matthew Zakari <zakarimatthew62@gmail.com>
2024-05-14 08:12:59 -0700

    python method correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL and displays the size of the response body in bytes
+# Sends a request to a URL and displays response size in bytes
 curl -s "$1" | grep -i Content-Length | awk '{print $2}
